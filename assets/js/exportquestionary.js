f47f1278930289d648581086c8da0b8575ab3bd8
CrymSonir <syrevenu@sfr.fr>
2019-01-28 17:23:43 +0100

    Fix issue with space encode and select second categ for export

--- assets/js/exportquestionary.js
+++ assets/js/exportquestionary.js
@@ -49,14 +49,15 @@ require(["core/ajax", "jquery", "jqueryui"], function (ajax, $) {
 });
 
 function exportToCsv(filename, rows) {
-    var processRow = function (row) {
-        var finalVal = '';
-        for (var j = 0; j < row.length; j++) {
-            var innerValue = row[j] === null ? '' : row[j].toString();
+    let processRow = function (row) {
+        let finalVal = '';
+        for (let j = 0; j < row.length; j++) {
+            let innerValue = row[j] === null ? '' : row[j].toString();
             if (row[j] instanceof Date) {
                 innerValue = row[j].toLocaleString();
             };
-            var result = innerValue.replace(/"/g, '""');
+            let result = innerValue.replace(/"/g, '""');
+            result = result.replace(/&nbsp;/g, ' ');
             if (result.search(/("|,|\n)/g) >= 0)
                 result = '"' + result + '"';
             if (j > 0)
@@ -66,19 +67,19 @@ function exportToCsv(filename, rows) {
         return finalVal + '\n';
     };
 
-    var csvFile = '';
+    let csvFile = '';
     for (var i = 0; i < rows.length; i++) {
         csvFile += processRow(rows[i]);
     }
 
-    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    let blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
     if (navigator.msSaveBlob) { // IE 10+
         navigator.msSaveBlob(blob, filename);
     } else {
-        var link = document.createElement("a");
+        let link = document.createElement("a");
         if (link.download !== undefined) { // feature detection
             // Browsers that support HTML5 download attribute
-            var url = URL.createObjectURL(blob);
+            let url = URL.createObjectURL(blob);
             link.setAttribute("href", url);
             link.setAttribute("download", filename);
             link.style.visibility = 'hidden';
